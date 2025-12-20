f5080f66044aa4ff93ee44a40204b0dd89cfb7b7
iceman2946 <110265043+iceman2946@users.noreply.github.com>
2025-12-20 22:29:51 +0000

    Projects (IP)

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -10,11 +10,25 @@ Write your own turtle program! Here is what your program should do
 """
 
 import turtle as turtle
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
 
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
 t = turtle.Turtle()
-
+set_turtle_image(t,"leaguebot_bolt.svg")
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+t.pencolor("blue")
+for i in range(6):
+    t.forward(100)
+    t.left(60)
 ... # Your Code Here
